28a63a676e789a9218429381760326016a6a51a2
Andrey Marques <andreymarques822@gmail.com>
2023-10-29 21:44:38 -0300

    Frontend: Aplicação da logica

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,14 @@ import "./Button.css"
 
 export default props => {
     return(
-        <button className={`
-            button
-            ${props.operation ? "operation": ""}
-            ${props.double ? "double" : ""}
-            ${props.triple ? "triple" : ""}
-        `}>
+        <button 
+        onClick={e => props.click && props.click(props.label)}
+            className={`
+                button
+                ${props.operation ? "operation": ""}
+                ${props.double ? "double" : ""}
+                ${props.triple ? "triple" : ""}
+            `}>
             {props.label}
         </button>
     )
